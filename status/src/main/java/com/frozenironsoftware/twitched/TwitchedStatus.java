095ffbda1d2903d7168ade6a041f20390051c935
Rolando Islas <admin@rolandoislas.com>
2018-06-16 09:58:15 -0700

    Add initial status build script, class, and procfile

---
+++ status/src/main/java/com/frozenironsoftware/twitched/TwitchedStatus.java
@@ -0,0 +1,11 @@
+package com.frozenironsoftware.twitched;
+
+import com.frozenironsoftware.twitched.data.Constants;
+import com.rolandoislas.twitchunofficial.util.Logger;
+
+public class TwitchedStatus {
+
+    public static void main(String[] args) {
+        Logger.info("%s version %s starting", Constants.NAME, Constants.VERSION);
+    }
+}
