2b50e5e1e42c89f397186150b864410cf1f1aaa0
Rolando Islas <admin@rolandoislas.com>
2018-06-26 16:58:23 -0700

    Up default cache expiry to 10 minutes from 3

--- server/src/main/java/com/rolandoislas/twitchunofficial/util/ApiCache.java
+++ server/src/main/java/com/rolandoislas/twitchunofficial/util/ApiCache.java
@@ -28,7 +28,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class ApiCache {
-    private static final int TIMEOUT = 60 * 3; // Seconds before a cache value should be considered invalid
+    private static final int TIMEOUT = 60 * 10; // Seconds before a cache value should be considered invalid
     public static final int TIMEOUT_HOUR = 60 * 60;
     public static final int TIMEOUT_DAY = 24 * 60 * 60; // 1 Day
     @SuppressWarnings("unused")
